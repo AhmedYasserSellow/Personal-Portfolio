24cb88bd7ba7ffc9ddbfcfa18d4ffbc6789f3fd8
Ahmed Yasser <aaysr201678@gmail.com>
2024-06-02 06:16:19 +0300

    fix scroll bars

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1669128414"
+    serviceWorkerVersion: "3870573413"
   }
 });
